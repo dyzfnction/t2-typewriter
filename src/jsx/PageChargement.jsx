290b4667ba05bf64e9bb4f3b3c003e6b0e85dd93
Seth <kimpeses@gmail.com>
2026-02-28 21:57:56 +0100

    ahhhhhhh

--- src/jsx/PageChargement.jsx
+++ src/jsx/PageChargement.jsx
@@ -10,43 +10,7 @@ export default function PageChargement({ onFinish }) { // loader
     const increment = 100 / (duration / intervalTime) // incrément
 
     const interval = setInterval(() => {
-      setCount(c => {import { useState, useEffect } from 'react' // hooks React
-
-export default function PageChargement({ onFinish }) { // loader
-  const [count, setCount] = useState(0) // compteur
-  const [exit, setExit] = useState(false) // état sortie
-
-  useEffect(() => {
-    const intervalTime = 40 // vitesse compteur
-    const duration = 4000 // durée totale
-    const increment = 100 / (duration / intervalTime) // incrément
-
-    const interval = setInterval(() => {
       setCount(c => {
-        const next = c + increment
-        if (next >= 100) {
-          clearInterval(interval)
-          setExit(true) // fade out
-          setTimeout(onFinish, 800) // fin loader
-          return 100
-        }
-        return next
-      })
-    }, intervalTime)
-
-    return () => clearInterval(interval)
-  }, [onFinish])
-
-  return (
-    <div className={`loader ${exit ? 'is-exit' : ''}`}>
-
-      <img src="../images/logo-the-marginalian.png" className="logo-bounce" alt="" />
-      <div className="shadow" />
-      <div className="counter">{Math.floor(count)}%</div>
-
-    </div>
-  )
-}
         const next = c + increment
         if (next >= 100) {
           clearInterval(interval)
